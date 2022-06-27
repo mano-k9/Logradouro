e96d16a625ffc28ca3933f3b49b28ce1d7559ec1
mano-k9 <93453179+mano-k9@users.noreply.github.com>
2022-06-27 16:08:25 -0300

    Update LogradouroNumero.py

--- LogradouroNumero.py
+++ LogradouroNumero.py
@@ -4,7 +4,7 @@ Created on Fri Jun 17 13:41:20 2022
 
 @author: mano-K9
 
-O algoritmo extrai o número e o complemento do endereco.
+O algoritmo extrai o logradouro, o número e o complemento do endereco.
 Nao há nada de AI ou ML neste codigo :)
 Usa os tipos basicos de logradouro na variável sLog1 (rua, avenida, estrada, etc.), e nao o dominio completo dos correios.
 Mas sinta-se a vontade para baixar em sua máquina e adaptar para atender a sua necessidade.
@@ -28,6 +28,7 @@ class Logradouro:
     self.endrco = endrco
   def numCmplto(self):
     sEndrc = self.endrco
+    endrco = sEndrc
     num = ''
     cmplto = ''
     if type(sEndrc) == str:
@@ -38,18 +39,23 @@ class Logradouro:
             spEndrco = sEndrco.split()
             i = 0
             sLog1 = '.ALAMEDA.AL.AVENIDA.AV.RUA.R.ESTRADA.EST.TREVO.TRV.TRAVESSA.TV.BULEVAR.BOULEVAR.BLV.TREVO.TRV.TRAVESSA.TV.'
-            sLog2 = '.RODOVIA.ROD.RD.BR.QUADRA.QD.CONJUNTO.CJ.APARTAMENTO.AP.APTO.KM.FAZENDA.FAZ.FZ.CHACARA.CHÁCARA.CHC.CHAC.SITIO.SÍTIO.ST.SIT.'
+            sLog2 = '.RODOVIA.ROD.RD.BR.KM.FAZENDA.FAZ.FZ.CHACARA.CHÁCARA.CHC.CHAC.SITIO.SÍTIO.ST.SIT.'
+            sComp = '.QUADRA.QD.CONJUNTO.CJ.APARTAMENTO.AP.APTO.CASA.C.CS.BLOCO.BL.BLCO.CONDOMINIO.COND.CD.'
             via = (sLog1.find(spEndrco[0]) > -1)
             naoVia = (sLog2.find(spEndrco[0]) > -1)
+            comp = False
             if via:
+                endrco = ''
                 for stEnd in spEndrco:
-                    if stEnd.isdigit():
-                        if i > 1:
-                            num = stEnd
-                            break
-                        if i > 1:
-                            num = stEnd
-                            break
+                    if not comp:
+                        comp = (sComp.find('.' + stEnd + '.') > -1)
+                    if comp:
+                        cmplto += (stEnd + ' ')
+                    elif stEnd.isdigit() and i > 0:
+                        num = stEnd
+                        break
+                    elif '.N.NO.NUM.NUMERO.NÚMERO.'.find('.' + stEnd + '.'):
+                        endrco += (stEnd + ' ')
                     i += 1
             elif naoVia:
                 num = ''
@@ -61,7 +67,12 @@ class Logradouro:
                             break                        
                     i += 1
     if num != '':
-        posNum = sEndrc.find(num) + len(num) + 1
-        cmplto = sEndrc[posNum:].strip()
-    return([num,cmplto])
+        posNum = sEndrc.find(num)
+        if endrco == sEndrc:
+            endrco = sEndrc[0:posNum].replace(',','').strip()
+        posNum += len(num) + 1
+        if cmplto == '':
+            cmplto = sEndrc[posNum:]
+    return([endrco.rstrip(),num,cmplto.rstrip()])
+
 
